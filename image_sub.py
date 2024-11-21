2b7f9a93a7f86778b417b56573638b65f8b4a46b
salvadoruriel <salvador.urielgm+git@gmail.com>
2024-11-21 18:13:00 +0800

    feat: 2024-11-21 code modifications in lab

--- image_sub.py
+++ image_sub.py
@@ -15,6 +15,30 @@ import numpy as np
 import math
 import argparse
 
+import os
+import time
+
+cwd = os.getcwd()
+print(f"cwd: {cwd}")
+outputFolder = f"{cwd}/src/send_script/send_script/output/"
+print(f"outputFolder: {outputFolder}")
+now = time.time()
+today = time.strftime('%Y-%m-%d',time.localtime(now))
+logfile = f"{today}-log.txt"
+
+def ourPrint(self='',string="",log=True):
+    now = time.time()
+    hour = time.strftime('%H:%M:%S',time.localtime(now))
+    
+    if self:
+        self.get_logger().info(string)
+    else:
+        print(string)
+    if not log: return
+    with open(f'{outputFolder}{logfile}', 'a') as file:
+        file.write(f'[{hour}] {string}\n')
+
+
 class ImageSub(Node):
     def __init__(self, nodeName):
         super().__init__(nodeName)
@@ -26,8 +50,11 @@ class ImageSub(Node):
         self.get_logger().info('Received image')
         bridge = CvBridge()
         image = bridge.imgmsg_to_cv2(data)
-        cv2.imwrite("/tmp/image.jpg",image)
-        self.get_logger().info("debug")
+        # cwd = os.getcwd()
+        # self.get_logger().info(f"directory> {cwd}")
+        # #creates the photo at top level folder, not cwd lol
+        # cv2.imwrite(f"{cwd}/output/image3.jpg",image)
+        # self.get_logger().info("debug")
 
 
         def calculate_centroid_and_angle(moment):
@@ -49,7 +76,8 @@ class ImageSub(Node):
         def process_image(image):
 
             height, width = image.shape[:2]
-
+            print(height)
+            print(width)
             # Crop 5% from each side
             crop_x = int(width * 0.05)
             crop_y = int(height * 0.05)
@@ -73,6 +101,7 @@ class ImageSub(Node):
             # Find contours
             contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+            #ourPrint(self,f'countours: {contours}',log=False)
             for contour in contours:
                 area = cv2.contourArea(contour)
 
@@ -91,29 +120,70 @@ class ImageSub(Node):
 
                 x = adjusted_centroid[0]
                 y = adjusted_centroid[1]
+                
+            
+                # Calculate extended line points based on the image dimensions
+                line_length = max(width, height) * 2  # Extend the line beyond the image size
+                x_end = int(adjusted_centroid[0] + line_length * math.cos(angle))
+                y_end = int(adjusted_centroid[1] + line_length * math.sin(angle))
+                x_start = int(adjusted_centroid[0] - line_length * math.cos(angle))
+                y_start = int(adjusted_centroid[1] - line_length * math.sin(angle))
+                
+                # Draw the infinite principal line with thickness 1
+                cv2.line(image, (x_start, y_start), (x_end, y_end), (255, 0, 0), 1)
+                
+                # Draw the centroid
+                cv2.circle(image, adjusted_centroid, 5, (0, 0, 255), -1)
+                
                 # Print the centroid and angle
+                ourPrint(self,f"Centroid: {adjusted_centroid}, Angle: {angle:.2f} radians")
+                
+                # # Display the final image with contours and angles for debugging
+                cv2.imshow("Result", image)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()   
+
+                # Print the centroid and angle
+                self.get_logger().info(f"here")
                 self.get_logger().info(f"Centroid: {adjusted_centroid}, Angle: {angle:.2f} radians")
-                return x,y, angle
+                if not x or not y or not angle:
+                    return 0,0,0
+                else:
+                    return x,y, angle
+            ourPrint(self,f'No centroids found. Maybe objects were not present in the camera range')
+            return 0,0,0 #
 
 
+        self.get_logger().info(f"Processing image")
+        try:
+            x,y,phi = process_image(image)
+        except Exception as e:
+            ourPrint(self,f"Something happened: {e}")
+            x,y,phi = 0,0,0
 
-        x,y,phi = process_image(image)
-        x1 = math.cos(math.radians(-45))*x/2
-        y1 = y/math.cos(math.radians(45))/2
-        phi1 = phi + 135.00
+        # x1 = math.cos(math.radians(50))*x/2
+        ycam = (((-y)+960))
+        x1=x*0.4497+ycam*0.2467 - 135
+        y1 = x*(-0.0705)+ycam*0.2467 + 360
+        #/math.cos(math.radians(50))
+        phi1 = math.degrees(phi) + 135.00
+        self.get_logger().info(f"{x1}{y1}{phi1}")
+        ourPrint(self,"converted values:")
+        ourPrint(self,f"x1: {x1}")
+        ourPrint(self,f"y1: {y1}")
+        ourPrint(self,f"phi1: {phi1}")
         # # 1 mm = 3 pixels
         # # 10 cm = 300 pixels
+        
+        # script = "PTP(\"CPP\","+targetP+",100,200,0,false)"
+        # send_script(script)
 
-        targetP = f"{x1}, {y1}, 500, -180.00, 0.0, {phi1}"
-        script = "PTP(\"CPP\","+targetP+",100,200,0,false)"
-        send_script(script)
-
-        # targetP1 = "230.00, 230, 730, -180.00, 0.0, 135.00"
-        #targetP2 = "280.00, 250, 500, -180.00, 0.0, 135.00"
-        # script1 = "PTP(\"CPP\","+targetP1+",100,200,0,false)"
-        #script2 = "PTP(\"CPP\","+targetP2+",100,200,0,false)"
-        # send_script(script1)
-        #send_script(script2)
+        x1=80
+        y1=100
+        targetP1 = f"{x1}, {y1}, 490, -180.00, 0.0, {phi1}"
+        #targetP1 = "250.00, 250, 190, -180.00, 0.0, 135.00"
+        script1 = "PTP(\"CPP\","+targetP1+",100,200,0,false)"
+        send_script(script1)
 ########################################################################################################################
 
 def send_script(script):
